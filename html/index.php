3dd82a4f178520fe70efeb67079315e145753d60
Raj kumar <rajkumar@10times.com>
2020-12-29 13:36:16 +0530

    changes

---
+++ html/index.php
@@ -0,0 +1,28 @@
+<?php
+// use Symfony\Component\Stopwatch\Stopwatch;
+
+// use Symfony\Component\ClassLoader\ApcClassLoader;
+// use Symfony\Component\HttpFoundation\Request;
+error_reporting(E_ALL);
+// print_r("expression");exit;
+// error_reporting(0);
+error_reporting(E_ERROR);
+ini_set('display_errors', 1);
+
+/* cross subdomain cookie sharing config */
+//ini_set('session.cookie_domain', '.10times.com');
+//session_set_cookie_params(0, '/', '.10times.com');
+// echo "sunil";exit;
+// echo "TESTING<BR>";
+//error_reporting(E_ALL);
+//var_dump($_GET);
+// echo "hello";exit;
+$root = '/home/indiamart/public_html/beta-biztradeshows';
+
+
+// $event = $stopwatch->stop('foo');
+// echo $event->getDuration()." miliseconds";
+
+
+
+?>
